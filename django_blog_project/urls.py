24c2d0a5f424aaed871f04a9e46179d4316577cc
avd151 <deshpandeav19.comp@coep.ac.in>
2022-07-21 17:05:50 +0530

    fix deployment bugs

--- django_blog_project/urls.py
+++ django_blog_project/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path, include, re_path
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import server
-from django.conf.urls import url
+from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,10 +30,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root':       settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_URL}),
 ]
 
 if settings.DEBUG:
